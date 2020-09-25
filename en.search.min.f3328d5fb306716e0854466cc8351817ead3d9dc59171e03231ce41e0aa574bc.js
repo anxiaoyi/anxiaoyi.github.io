65c75725fdc2321263e3a035516165e1d902aefe
Zhao Kun <igozhaokun@163.com>
2020-09-25 17:40:10 +0800

    update

--- en.search.min.f3328d5fb306716e0854466cc8351817ead3d9dc59171e03231ce41e0aa574bc.js
+++ en.search.min.f3328d5fb306716e0854466cc8351817ead3d9dc59171e03231ce41e0aa574bc.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.199fb296416e407dd523d06921f3af18ffed2c9a3677a35e1df65c6cc26e6328.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.4d517ff9088fd6188b000493091573022a016ecea5e558bdfe45cf5eb517f9d3.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
