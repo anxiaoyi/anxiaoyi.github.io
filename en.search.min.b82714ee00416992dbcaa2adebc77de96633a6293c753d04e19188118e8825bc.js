9234df91ced38dd3402a2060ddc443ddaf7b67ee
KunZhao <igozhaokun@163.com>
2020-12-28 23:03:50 +0800

    rebuild site

--- en.search.min.b82714ee00416992dbcaa2adebc77de96633a6293c753d04e19188118e8825bc.js
+++ en.search.min.b82714ee00416992dbcaa2adebc77de96633a6293c753d04e19188118e8825bc.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c1550e0c46f0e096ccbb6356409915b31ea8d98c4e5b7778d37b551b03ee5d01.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.78d2f70675f56237c05c2960aca6b883dff585bc14cf6ca635b973901930297c.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
